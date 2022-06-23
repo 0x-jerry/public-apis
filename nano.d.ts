1302d3e3dc6de9068a079b5e597564a123b2f070
Jerry Wang <x.jerry.wang@gmail.com>
2022-06-23 13:09:45 +0800

    fix: access file api

---
+++ nano.d.ts
@@ -0,0 +1,36 @@
+// fix JSX typedef, source address: https://deno.land/x/nano_jsx@v0.0.32/core.types.ts
+
+// fixes an issue in std@0.80.0 (deno)
+// interface ReadableStream<R> {
+//   getIterator(): any
+// }
+
+declare var _nano: {
+  document: Document
+  isSSR: true | undefined
+  location: { pathname: string }
+  customElements: Map<string, any>
+  ssrTricks: {
+    isWebComponent: (tagNameOrComponent: any) => boolean
+    renderWebComponent: (
+      tagNameOrComponent: any,
+      props: any,
+      children: any,
+      _render: any
+    ) => any
+  }
+}
+
+declare namespace Deno {}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any
+  }
+  interface ElementClass {
+    render: any
+  }
+  interface ElementChildrenAttribute {
+    children: any
+  }
+}
