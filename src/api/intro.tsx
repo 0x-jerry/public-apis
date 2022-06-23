eebfefcbf1aeda8b12aa4b77bf71042b9c95d6a6
Jerry Wang <x.jerry.wang@gmail.com>
2022-06-23 13:05:42 +0800

    fix: style

--- src/api/intro.tsx
+++ src/api/intro.tsx
@@ -1,6 +1,7 @@
 import { h, renderSSR } from 'https://deno.land/x/nano_jsx@v0.0.32/mod.ts'
 
 import * as gfm from 'https://deno.land/x/gfm@0.1.20/mod.ts'
+import 'https://esm.sh/prismjs@1.28.0/components/prism-typescript?no-check'
 
 function App() {
   const intro = Deno.readTextFileSync('./readme.md')
@@ -11,12 +12,22 @@ function App() {
       <head>
         <title>Deno Deploy API</title>
       </head>
-      <style>{gfm.CSS}</style>
+      <style>
+        {`html,body {padding: 0;margin: 0;}`}
+
+        {gfm.CSS}
+      </style>
       <body>
         <div
-          style={{ width: '768px', margin: 'auto' }}
-          dangerouslySetInnerHTML={{ __html: md }}
-        ></div>
+          data-color-mode="auto"
+          data-dark-theme="auto"
+          class="markdown-body"
+        >
+          <div
+            style={{ width: '768px', margin: 'auto' }}
+            dangerouslySetInnerHTML={{ __html: md }}
+          ></div>
+        </div>
       </body>
     </html>
   )
