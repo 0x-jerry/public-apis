f5bad3e0b0e982e917835dfb0aad9499d66d30e3
Jerry <x.jerry.wang@gmail.com>
2026-05-30 15:31:47 +0800

    adjust middleware

--- src/_app.ts
+++ src/_app.ts
@@ -1,26 +1,3 @@
 import { Hono } from 'hono'
-import { auth } from './_auth.ts'
 
 export const app = new Hono()
-
-app.use(auth)
-
-app.use(async (ctx, next) => {
-  const start = Date.now()
-  await next()
-  const ms = Date.now() - start
-  console.log(`${ctx.req.method} ${ctx.req.path} ${ctx.res.status} ${ms}ms`)
-})
-
-app.use(async (ctx, next) => {
-  await next()
-
-  if (ctx.error) {
-    console.error(ctx.error)
-
-    ctx.status(400)
-    ctx.res = ctx.json({
-      message: String(ctx.error),
-    })
-  }
-})
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
+import { logger } from 'hono/logger'
 import { app } from './_app.ts'
 import './_index.tsx'
-
+import { auth } from './_auth.ts'
 import { app as qrRouter } from './qr/index.ts'
 import { app as svgRouter } from './svg/index.ts'
 import { app as htmlRouter } from './html/index.ts'
 import { app as imgToAsciiRouter } from './img-to-ascii/index.ts'
 import { app as mcpRouter } from './mcp/index.ts'
 
+app.use(logger())
+
+app.use(async (ctx, next) => {
+  await next()
+
+  if (ctx.error) {
+    console.error(ctx.error)
+
+    ctx.status(400)
+    ctx.res = ctx.json({
+      message: String(ctx.error),
+    })
+  }
+})
+
 app.route('/qr', qrRouter)
 app.route('/svg', svgRouter)
+
+app.use('/html/*', auth)
 app.route('/html', htmlRouter)
+
 app.route('/img-to-ascii', imgToAsciiRouter)
+
+app.use('/mcp/*', auth)
 app.route('/mcp', mcpRouter)
 
 export { app }
