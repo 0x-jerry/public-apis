a76b901b874e094901a5bf1ca7c54c6e0b8d5622
Jerry <x.jerry.wang@gmail.com>
2026-06-01 09:36:19 +0000

    format code

--- examples/browser.ts
+++ examples/browser.ts
@@ -1,22 +1,27 @@
-import { getBrowser } from "../src/_libs/browser";
-import { htmlToMarkdown } from "../src/_libs/html-to-markdown";
-import { sleep } from "@0x-jerry/utils";
+import { getBrowser } from "../src/_libs/browser"
+import { htmlToMarkdown } from "../src/_libs/html-to-markdown"
+import { sleep } from "@0x-jerry/utils"
 
-const browser = await getBrowser();
+const browser = await getBrowser()
 if (!browser) {
-  throw new Error("Browser not connected!");
+  throw new Error("Browser not connected!")
 }
 
-const page = await browser.newPage();
+const page = await browser.newPage()
 
 const url = "https://news.ycombinator.com/news";
-await page.goto(url, { waitUntil: "networkidle0" });
 
-const content = await page.content();
+await Promise.race([
+  // 
+  page.goto(url, { waitUntil: "networkidle0" }),
+  sleep(20_000),
+])
 
-const md = htmlToMarkdown(content);
+const content = await page.content()
 
-console.log(md);
+const md = htmlToMarkdown(content)
 
-await page.close();
-await browser.close();
+console.log(md)
+
+await page.close()
+await browser.close()
--- examples/img-to-ascii.ts
+++ examples/img-to-ascii.ts
@@ -1,6 +1,6 @@
-import { readFileSync } from 'node:fs';
-import { imgToAscii } from '../src/_libs/img-to-ascii/index.ts';
+import { readFileSync } from "node:fs"
+import { imgToAscii } from "../src/_libs/img-to-ascii/index.ts"
 
-const buffer = readFileSync('assets/deepseek.png');
-const ascii = await imgToAscii(new Uint8Array(buffer), { maxWidth: 80, maxHeight: 40 });
-console.log(ascii);
+const buffer = readFileSync("assets/deepseek.png")
+const ascii = await imgToAscii(new Uint8Array(buffer), { maxWidth: 80, maxHeight: 40 })
+console.log(ascii)
